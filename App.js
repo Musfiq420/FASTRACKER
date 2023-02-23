5bb08520faa95936f79ec4f7e0edd5ae25375b9c
@Musfiq420 <sfl-ws@squaregroup.com>
2023-02-23 15:10:57 +0600

    machine tracking system added

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import {NavigationContainer} from "@react-navigation/native"
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
+import { createDrawerNavigator } from '@react-navigation/drawer'
 import IntroScreen from './Screens/IntroScreen';
 import SquareNews from './Screens/squareNews';
 import HourlyProductionContainer from './Screens/HourlyProductionContainer';
@@ -9,6 +9,7 @@ import MachineOptimizationContainer from './Screens/MachineOptimizationContainer
 import CapacityAnalysisContainer from './Screens/CapacityAnalysisContainer';
 
 const Stack =createNativeStackNavigator()
+const Drawer = createDrawerNavigator();
 
 export default function App() {
 
@@ -16,6 +17,13 @@ export default function App() {
     <>
       <StatusBar style='dark'/>
       <NavigationContainer >
+      {/* <Drawer.Navigator useLegacyImplementation initialRouteName="Home">
+        <Drawer.Screen name="HOME" component={IntroScreen} />
+        <Drawer.Screen name="HOURLY PRODUCTION CONTAINER" component={HourlyProductionContainer} />
+        <Drawer.Screen name="MACHINE OPTIMIZATION" component={MachineOptimizationContainer} />
+        <Drawer.Screen name="CAPACITY ANALYSIS" component={CapacityAnalysisContainer} />
+        <Drawer.Screen name="SQUARE NEWS" component={SquareNews}/>
+      </Drawer.Navigator> */}
         <Stack.Navigator screenOptions={{animation: 'simple_push', headerStyle:{backgroundColor: "#9fff8c"}}}>
           <Stack.Screen options={{headerShown:false}} name="HOME" component={IntroScreen}/>
           <Stack.Screen name="HOURLY PRODUCTION CONTAINER" component={HourlyProductionContainer} options={{title:"SEWING PRODUCTION"}}/>
--- Screens/BreakdownScanner.js
+++ Screens/BreakdownScanner.js
@@ -1,29 +1,101 @@
 import axios from "axios"
+import { addDoc, collection, doc, getDoc, setDoc } from "firebase/firestore"
 import { useEffect, useState } from "react"
 import { StyleSheet, Text, TouchableOpacity, View, Modal, TextInput, Button } from "react-native"
 import SwitchSelector from "react-native-switch-selector"
 import BarCode from "../Components/BarcodeScanner"
+import { fireStoreDb } from "../lib/firebase"
 
 function BreakdownScanner(){
+    const today = new Date();
+    const dateString = `${today.getFullYear()}-${(today.getMonth()+1)<10?'0'+(today.getMonth()+1):(today.getMonth()+1)}-${today.getDate()<10?'0'+today.getDate():today.getDate()}`;
+
     const [scannerOpen, setScannerOpen] = useState(false)
     const [data, setData] = useState('');
     const [openModal, setOpenModal] = useState(false);
-    const [breakdownType, setBreakdownType] = useState("start")
+
+    const [line, setLine] = useState(null);
+    const [problem, setProblem] = useState('');
+    const [status, setStatus] = useState('');
+
+
+
+    // const [breakdownType, setBreakdownType] = useState("start")
     
-    const options = [
-        { label: "Start", value: "start", activeColor: 'green'},
-        { label: "End", value: "end", activeColor: 'green'}
-    ];
+    // const options = [
+    //     { label: "Start", value: "start", activeColor: 'green'},
+    //     { label: "End", value: "end", activeColor: 'green'}
+    // ];
 
 
     useEffect(() => {
         console.log('data useEffect called');
-        setOpenModal(true);
-        setScannerOpen(false);
+        if(data)
+        {
+            getStatusOfMachine(data);
+        }
     }, [data])
 
+    const getStatusOfMachine = async(data) => {
+        const docRef = doc(fireStoreDb, "machine-info", data);
+        const docSnap = await getDoc(docRef);
+        setStatus(docSnap.data().status);
+        setOpenModal(true);
+        setScannerOpen(false);
+    }
+
     const sendDataToServer = async(data) => {
-        await axios.patch('https://firsttrial-cff1d-default-rtdb.firebaseio.com/machineBreakdown.json', {"scannedData":data})
+        const docRef = doc(fireStoreDb, "machine-info", data);
+        const docSnap = await getDoc(docRef);
+        if(docSnap.data().status === 'ok')
+        {
+            await setDoc(docRef, {
+                "last breakdown": new Date(),
+                status: 'repairing'
+            }, { merge:true })
+
+        }
+        else if(docSnap.data().status === 'repairing')
+        {
+            await setDoc(docRef, {
+                line: Number(line),
+                status: 'ok'
+            }, { merge:true })
+
+            await addDoc(collection(fireStoreDb,"machine-lost-time"), {
+                id: data, 
+                date: new Date(dateString),
+                lost: Math.round(((new Date()).getTime() - docSnap.data()["last breakdown"].toDate().getTime())/1000), 
+                line: Number(line), 
+                problem: problem
+            })
+        }
+        // console.log(docSnap.data().status)
+        
+        // const res = await addDoc(collection(fireStoreDb,"machine-lost-time"), {
+        //     id: data, 
+        //     date: new Date(), 
+        //     lost: 36, 
+        //     line: Number(line), 
+        //     problem: problem
+        // })
+        // console.log("Document written with ID: ", res.id);
+        // await axios.patch('https://firsttrial-cff1d-default-rtdb.firebaseio.com/machineBreakdown.json', {"scannedData":data})
+        // await axios.post(`https://firestore.googleapis.com/v1/projects/firsttrial-cff1d/databases/(default)/documents/machine-lost-time`, 
+        // { fields: 
+        //     { 
+        //         id: { stringValue: data }, 
+        //         date: { timestampValue: new Date() }, 
+        //         lost: { integerValue: 67 }, 
+        //         line: { integerValue: line }, 
+        //         problem: { stringValue: problem }
+        //     } 
+        // }
+        // )
+        setOpenModal(false);
+        setData("");
+        setLine(null);
+        setProblem("");
     }
 
     return(
@@ -35,31 +107,41 @@ function BreakdownScanner(){
             onRequestClose={() => {
             setOpenModal(false);
             setScannerOpen(false);
+            setData("");
+            setLine(null);
+            setProblem("");
             }}
             >
                 <View style={styles.centeredView}>
                     <View style={styles.modalView}>
-                        <Text style={styles.modalText}>Machine Breakdown</Text>
-                        <TextInput placeholder="Line no" />
-                        <Text>Machine ID: {data}</Text>
-                        <Text>Machine Name: Overlock</Text>
-                        <Text>Company: Juki</Text>
-                        <View style={{height:50, width:200 }}>
-                            <SwitchSelector
-                                style={{paddingBottom:20}}
-                                options={options}
-                                initial={0}
-                                onPress={setBreakdownType}  
-                                backgroundColor='#c2f8cb'
-                            />
+                        <Text style={{fontSize:20}}>ID: {data} is {status}</Text>
+                        
+                        {status==='ok'?null:
+                        <>
+                        <View style={{display:'flex', flexDirection:'column', justifyContent:'flex-start', margin:20}}>
+                            <View style={{display:'flex', flexDirection:'row', alignItems:'center'}}>
+                                <Text style={{width:70}}>Line No:</Text><TextInput value={line} placeholder="line" keyboardType='numeric' onChangeText={setLine} style={{borderWidth:1, width:150, borderColor:'gray', borderRadius:5, paddingHorizontal:5, margin:5}} />
+                            </View> 
+                            <View style={{display:'flex', flexDirection:'row', alignItems:'center'}}>
+                                <Text style={{width:70}}>Problem:</Text><TextInput value={problem} onChangeText={setProblem} placeholder="problem" style={{borderWidth:1, width:150, borderColor:'gray', borderRadius:5, paddingHorizontal:5, margin:5}} />
+                            </View>
                         </View>
-                        <View style={styles.button}>
+                        
+                        </>
+                        }
+                        
+                        <View style={{ margin:10, padding:10 }}>
+                            <TouchableOpacity style={{backgroundColor:'green', padding:15, borderRadius:20}} onPress={() => sendDataToServer(data)}>
+                                <Text style={{color:'white'}} >{status==='ok'?"Start Breakdown":"Finish Breakdown"}</Text> 
+                            </TouchableOpacity>
+                        </View>
+                        {/* <View style={styles.button}>
                         <Button  title="Submit" color={'green'} onPress={async() => {
                             await sendDataToServer(data);
                             setScannerOpen(false);
                             setOpenModal(false);
                             }} />
-                        </View>
+                        </View> */}
                         
                     </View>
                 </View>
